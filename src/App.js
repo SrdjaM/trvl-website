96b79e2bcce6130c1dd1c32d45d6b75afd4d2112
SrdjaM <srdjan.mijatovic87@gmail.com>
2022-07-13 11:24:58 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/services" exact element={<Services />} />
+          <Route path="/products" exact element={<Products />} />
+          <Route path="/sign-up" exact element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
